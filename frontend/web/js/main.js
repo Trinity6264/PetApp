72daed6dae6960871dd6ed538f4c2a2d0f4239bd
Trinity <amoahtnt@gmail.com>
2022-08-10 00:50:28 +0000

    Done with landing page

---
+++ frontend/web/js/main.js
@@ -0,0 +1,8 @@
+
+let btn = document.getElementsByClassName("btn");
+
+btn.innerText = 'Hello Play';
+btn.addEventListener("click", (e) => {
+  alert("Hello word");
+  console.log("hello world");
+});
